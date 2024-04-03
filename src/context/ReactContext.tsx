83ae93409e2cc0996e5fc655c486657ba8854557
marioxaxa <66087369+marioxaxa@users.noreply.github.com>
2024-04-03 12:05:23 -0300

    Atualizações do endereço do back no contexto

--- src/context/ReactContext.tsx
+++ src/context/ReactContext.tsx
@@ -46,7 +46,7 @@ function ReactContext({ children }: Props) {
     const { data: roomList } = useQuery({
         queryKey: ["roomListContext"],
         queryFn: async () => {
-            const response = await axios.get("http://localhost:8080/room/list");
+            const response = await axios.get("http://localhost:8087/room/list");
             return response.data;
         },
     });
@@ -54,7 +54,7 @@ function ReactContext({ children }: Props) {
     const { data: userList } = useQuery({
         queryKey: ["userListContext"],
         queryFn: async () => {
-            const response = await axios.get("http://localhost:8080/user/list");
+            const response = await axios.get("http://localhost:8087/user/list");
             return response.data;
         },
     });
@@ -63,7 +63,7 @@ function ReactContext({ children }: Props) {
         queryKey: ["reservationListContext"],
         queryFn: async () => {
             const response = await axios.get(
-                "http://localhost:8080/reservation/list"
+                "http://localhost:8087/reservation/list"
             );
             return response.data;
         },
@@ -73,7 +73,7 @@ function ReactContext({ children }: Props) {
         queryKey: ["keyListContext"],
         queryFn: async () => {
             const response = await axios.get(
-                "http://localhost:8080/keydelivery/list"
+                "http://localhost:8087/keydelivery/list"
             );
             return response.data;
         },
