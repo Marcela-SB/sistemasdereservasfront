447ff754775320d42acbb2efaecb82c22d6b9986
marioxaxa <66087369+marioxaxa@users.noreply.github.com>
2025-02-24 03:47:22 -0300

    proxy

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -6,10 +6,12 @@ import axios from 'axios'
  * 
  * url dev
  * baseURL:"http://localhost:8080/"
+ * 
+ * 
  */
 
 const axiosInstance = axios.create({
-    baseURL:"http://10.3.227.44:8087/"
+    baseURL:"https://salas.deart.ufrn.br/api/"
 })
 
 export default axiosInstance
