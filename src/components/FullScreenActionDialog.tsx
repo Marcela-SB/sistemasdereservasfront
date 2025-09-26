99562c2f32ae1c54c0413a64ff329df7f4ccf373
Marcela <marcela013sb@hotmail.com>
2025-09-25 22:44:53 -0300

    [fix] Tela branca após codeStatus 500 (paliativo)

--- src/components/FullScreenActionDialog.tsx
+++ src/components/FullScreenActionDialog.tsx
@@ -199,7 +199,8 @@ export default function FullScreenActionDialog({
             setSnackBarSeverity("success");
         },
         onError: (error) => {
-            setSnackBarText(error.response.data);
+            const errorMessage = error.response.data || error.response?.data?.error || "Ocorreu um erro desconhecido.";
+            setSnackBarText(errorMessage);
             setSnackBarSeverity("error");
         },
     });
